145ef22d8705f4d0ff085f4d8caaa0fbba900369
Vinaya2092000 <vinaya2srinivasan@gmail.com>
2022-08-31 20:14:06 +0530

    [Vinaya]Add: Check Attendance

---
+++ EmployeeProblems/EmpPresent.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeProblems
+{
+    public class EmpPresent
+    {
+        public void Present()
+        {
+            int FULL_TIME = 1;
+            Random random = new Random();
+            int attendCheck = random.Next(0, 2);
+            if (attendCheck == FULL_TIME)
+            {
+                Console.WriteLine("The Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("The Employee is absent");
+            }
+        }
+    }
+}
---
+++ EmployeeProblems/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace EmployeeProblems
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1 for Attendence");
+            Console.WriteLine("2 for EmpWage for full time");
+            Console.WriteLine("3 for EmpWage for part time");
+            Console.WriteLine("4 for Using switch case");
+            Console.WriteLine("5 for 20 Working Day per Month");
+            Console.WriteLine("6 for 100 hours and 20 days");
+            Console.WriteLine("7 for  Use Class Method and Class Variables");
+            int sh = Convert.ToInt32(Console.ReadLine());
+            NewMethod(sh);
+        }
+
+        private static void NewMethod(int sh)
+        {
+            switch (sh)
+            {
+                case 1:
+                    EmpPresent empPresent = new EmpPresent();
+                    empPresent.Present();
+                    break;
+            }
+        }
+    }
+}
